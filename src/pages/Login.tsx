8569936862bea27a357bf47d7d919364574ed2d9
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-01-14 18:44:56 -0500

    feat: add navigation links to Login and Register pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <div>Login</div>;
+      <nav>
+        <Link to="/auth/register">No tienes cuenta? Crea una aqui</Link>
+      </nav>
+    </>
+  );
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Register() {
-  return <div>Register</div>;
+  return (
+    <>
+      <div>Register</div>;
+      <nav>
+        <Link to="/auth/login">Ya tienes una cuenta? Inicia Sesion</Link>
+      </nav>
+    </>
+  );
 }

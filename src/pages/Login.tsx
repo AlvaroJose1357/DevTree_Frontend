02f57121ce0aa548f357c2d696adde1e89460268
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-02-24 20:17:33 -0500

    fix: remove unnecessary semicolons from Login and Register components

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Login() {
   return (
     <>
-      <div>Login</div>;
+      <div>Login</div>
       <nav>
         <Link to="/auth/register">No tienes cuenta? Crea una aqui</Link>
       </nav>

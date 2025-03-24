2b1887027c8d0aee5fb99508eb5654b0f1ef1635
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-03-23 22:28:26 -0500

    feat(profile): add form for editing user information with handle, description, and image upload

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,3 +1,43 @@
 export default function Profile() {
-  return <div>Profile</div>;
+  return (
+    <form className="space-y-5 rounded-lg bg-white p-10" onSubmit={() => {}}>
+      <legend className="text-center text-2xl text-slate-800">
+        Editar Información
+      </legend>
+      <div className="grid grid-cols-1 gap-2">
+        <label htmlFor="handle">Handle:</label>
+        <input
+          type="text"
+          className="rounded-lg border-none bg-slate-100 p-2"
+          placeholder="handle o Nombre de Usuario"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 gap-2">
+        <label htmlFor="description">Descripción:</label>
+        <textarea
+          className="rounded-lg border-none bg-slate-100 p-2"
+          placeholder="Tu Descripción"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 gap-2">
+        <label htmlFor="handle">Imagen:</label>
+        <input
+          id="image"
+          type="file"
+          name="handle"
+          className="rounded-lg border-none bg-slate-100 p-2"
+          accept="image/*" // Solo acepta archivos de imagen
+          onChange={() => {}}
+        />
+      </div>
+
+      <input
+        type="submit"
+        className="w-full cursor-pointer rounded-lg bg-cyan-400 p-2 text-lg font-bold uppercase text-slate-600"
+        value="Guardar Cambios"
+      />
+    </form>
+  );
 }

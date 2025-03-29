e92e666315288bedeeb81009c1b4a1f1c77b2746
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-03-29 00:12:00 -0500

    feat(Profile): implement form handling for user profile editing with validation, and caching for fetching data

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,36 @@
+import { useForm } from "react-hook-form";
+import Error from "../components/Error";
+import { useQueryClient } from "@tanstack/react-query";
+import { User, UserProfile } from "../types";
+
 export default function Profile() {
+  // para la obtencion de datos cacheados de la query
+  // useQueryClient es un hook que nos permite acceder al cliente de la query
+  const queryClient = useQueryClient();
+  // getQueryData es un metodo que nos permite obtener los datos cacheados de la query, ["user"] es el nombre de la query que se guardo en el cliente de la query, se le coloca el ! al final ya que estamos haciendo la validacion en applayout.tsx de que si no existe el usuario, si este [user] no existe, va a ir hacia el useQuery de aplayout.tsx
+  const data: User = queryClient.getQueryData(["user"])!;
+
+  const INITIAL_VALUES = {
+    handle: data?.handle,
+    description: data?.description,
+  };
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<UserProfile>({ defaultValues: INITIAL_VALUES });
+
+  const handleProfile = (formData: UserProfile) => {
+    console.log("Form data", formData);
+
+    reset();
+  };
   return (
-    <form className="space-y-5 rounded-lg bg-white p-10" onSubmit={() => {}}>
+    <form
+      className="space-y-5 rounded-lg bg-white p-10"
+      onSubmit={handleSubmit(handleProfile)}
+    >
       <legend className="text-center text-2xl text-slate-800">
         Editar Información
       </legend>
@@ -10,7 +40,11 @@ export default function Profile() {
           type="text"
           className="rounded-lg border-none bg-slate-100 p-2"
           placeholder="handle o Nombre de Usuario"
+          {...register("handle", {
+            required: "El handle es obligatorio",
+          })}
         />
+        {errors.handle && <Error>{errors.handle.message}</Error>}
       </div>
 
       <div className="grid grid-cols-1 gap-2">
@@ -18,7 +52,11 @@ export default function Profile() {
         <textarea
           className="rounded-lg border-none bg-slate-100 p-2"
           placeholder="Tu Descripción"
+          {...register("description", {
+            required: "La descripción es obligatoria",
+          })}
         />
+        {errors.description && <Error>{errors.description.message}</Error>}
       </div>
 
       <div className="grid grid-cols-1 gap-2">

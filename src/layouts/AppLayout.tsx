5b36da1718872c87c31365b0b5d75cb0cd9f15cc
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-03-28 23:38:01 -0500

    refactor: user data handling and introduce DevTree component for layout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,8 +1,7 @@
-import { Link, Navigate, Outlet } from "react-router-dom";
-import { Toaster } from "sonner";
+import { Navigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import NavigationTabs from "../components/NavigationTabs";
 import { getUser } from "../api/DevTreeAPI";
+import DevTree from "../components/DevTree";
 
 export default function AppLayout() {
   const {
@@ -37,48 +36,5 @@ export default function AppLayout() {
 
   if (isError) return <Navigate to={"/auth/login"} />;
 
-  return (
-    <>
-      <header className="bg-slate-800 py-5">
-        <div className="mx-auto flex max-w-5xl flex-col items-center md:flex-row md:justify-between">
-          <div className="w-full p-5 md:w-1/3 lg:p-0">
-            <img src="/logo.svg" className="block w-full" />
-          </div>
-          <div className="md:flex md:w-1/3 md:justify-end">
-            <button
-              className="cursor-pointer rounded-lg bg-lime-500 p-2 text-xs font-black uppercase text-slate-800"
-              onClick={() => {}}
-            >
-              Cerrar Sesión
-            </button>
-          </div>
-        </div>
-      </header>
-      <div className="min-h-screen bg-gray-100 py-10">
-        <main className="mx-auto max-w-5xl p-10 md:p-0">
-          <NavigationTabs />
-          <div className="flex justify-end">
-            <Link
-              className="text-right text-2xl font-bold text-slate-800"
-              to={""}
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              Visitar Mi Perfil
-            </Link>
-          </div>
-
-          <div className="mt-10 flex flex-col gap-10 md:flex-row">
-            <div className="flex-1">
-              <Outlet />
-            </div>
-            <div className="w-full space-y-6 bg-slate-800 px-5 py-10 md:w-96">
-              {/* aqui va el drag and drop */}
-            </div>
-          </div>
-        </main>
-      </div>
-      <Toaster position="top-right" />
-    </>
-  );
+  if (data) return <DevTree data={data} />;
 }

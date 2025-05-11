d5a1e40cac1942a6e6cf5f2670311c2158e60f44
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-05-10 23:55:15 -0500

    feat: implement logout functionality in AdminNavigation component

--- src/components/nav/AdminNavigation.tsx
+++ src/components/nav/AdminNavigation.tsx
@@ -1,8 +1,15 @@
+import { useQueryClient } from "@tanstack/react-query";
 export default function AdminNavigation() {
+  const queryClient = useQueryClient();
+  const logout = () => {
+    // Invalidate all queries to refetch data
+    queryClient.invalidateQueries({ queryKey: ["user"] });
+    localStorage.removeItem("AUTH_TOKEN");
+  };
   return (
     <button
       className="cursor-pointer rounded-lg bg-lime-500 p-2 text-xs font-black uppercase text-slate-800"
-      onClick={() => {}}
+      onClick={logout}
     >
       Cerrar Sesión
     </button>
